80fbad663df93a9ea50304cffb785832b98878a6
Bernadett Urban <bernadett.urban@outlook.com>
2023-09-30 07:32:36 +0200

    chore: reformat code

--- Task2/src/test/java/pastebintest/pages/StartPage.java
+++ Task2/src/test/java/pastebintest/pages/StartPage.java
@@ -1,6 +1,9 @@
 package pastebintest.pages;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,7 +22,7 @@ public class StartPage {
     private static final String SYNTAX = "bash";
     private final WebDriver driver;
 
-    JavascriptExecutor js;
+    // JavascriptExecutor js;
 
 
     @FindBy(xpath = "//*[@id='postform-name']")
@@ -28,7 +31,7 @@ public class StartPage {
     List<WebElement> submitButton;
     @FindBy(xpath = "//div[@class = 'toggle__control']")
     WebElement toggleControl;
-    @FindBy(xpath = "//*[@id='postform-text']")
+    @FindBy(xpath = "//*[@class = 'textarea -form js-paste-code']")
     private WebElement pasteTextArea;
 
     public StartPage(WebDriver driver) {
@@ -56,32 +59,33 @@ public class StartPage {
     }
 
     public StartPage toogleSyntaxSwitch() {
+        FluentWait wait = new FluentWait(driver);
+        wait.withTimeout(Duration.ofSeconds(4000));
+        wait.pollingEvery(Duration.ofMillis(100));
         WebElement closeCookie = driver.findElement(By.xpath("//span[@class = 'cookie-button js-close-cookies']"));
         closeCookie.click();
+        wait.withTimeout(Duration.ofSeconds(4000));
+        wait.pollingEvery(Duration.ofMillis(100));
 
         WebElement closeOtherCookie = driver.findElement(By.xpath("//div[@title = 'Close Me']"));
         closeOtherCookie.click();
-
-
         toggleControl.click();
-
-
         return this;
     }
 
-    public StartPage selectSyntax(){
-
-        WebElement syntaxHighLightDropdown = driver.findElement(By.xpath("//span[@id = 'select2-postform-format-container']"));
+    public StartPage selectSyntax() {
+        WebElement syntaxHighLightDropdown = driver.findElement
+                (By.xpath("//span[@id = 'select2-postform-format-container']"));
         syntaxHighLightDropdown.click();
-        WebElement inputSyntaxField = driver.findElement(By.xpath("// input [@class='select2-search__field']"));
 
+        WebElement inputSyntaxField = driver.findElement(By.xpath("// input [@class='select2-search__field']"));
         inputSyntaxField.sendKeys(SYNTAX);
         inputSyntaxField.sendKeys(Keys.ENTER);
         return this;
-
     }
 
     public StartPage writeCodeInInputField() {
+        pasteTextArea.click();
         pasteTextArea.sendKeys(CODE);
         return this;
     }
@@ -91,6 +95,7 @@ public class StartPage {
 
         WebElement expiryContainer = driver.findElement(By.id("select2-postform-expiration-container"));
         expiryContainer.click();
+
         Actions actions = new Actions(driver);
         WebElement option = driver.findElement(By.xpath("//ul[contains(@class, 'select2-results__options')]/li[3]"));
         actions.moveToElement(option).click().perform();
@@ -103,9 +108,10 @@ public class StartPage {
 
     }
 
-    public void submitPaste() {
+    public StartPage submitPaste() {
 
         submitButton.get(0).click();
+        return this;
     }
 
 }
