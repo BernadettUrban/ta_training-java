9185a38b9ac6d74f49fcc2f0431cfdd062e5b58f
Bernadett Urban <bernadett.urban@outlook.com>
2023-10-05 16:07:44 +0200

    change timeout

--- Task3/src/test/java/calculatortest/googlepricecalculatorpages/AbstractPage.java
+++ Task3/src/test/java/calculatortest/googlepricecalculatorpages/AbstractPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public abstract class AbstractPage {
-    protected final int WAIT_TIMEOUT_SECONDS = 30;
+    protected final int WAIT_TIMEOUT_SECONDS = 10;
     protected WebDriver driver;
     protected StringUtils stringUtils = new StringUtils();
 
