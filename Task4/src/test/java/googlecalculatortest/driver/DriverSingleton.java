35698c04dfd0664e73a27b64764cae4f908b8755
Bernadett Urban <bernadett.urban@outlook.com>
2023-10-09 19:52:08 +0200

    edge driver setup

--- Task4/src/test/java/googlecalculatortest/driver/DriverSingleton.java
+++ Task4/src/test/java/googlecalculatortest/driver/DriverSingleton.java
@@ -4,6 +4,7 @@ import googlecalculatortest.service.PropertyFileReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
@@ -17,9 +18,9 @@ public class DriverSingleton {
     public static WebDriver getDriver() {
         if (null == driver) {
             switch (PropertyFileReader.getEnvironment(BROWSER)) {
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver= new EdgeDriver();
                     break;
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
